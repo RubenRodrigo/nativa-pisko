7a13a264af7c618be0e323f39be6bc1cb4c13c4d
Ruben Rodrigo <ruben.chani@tecsup.edu.pe>
2021-02-07 12:21:54 -0500

    Vista/Estilos Contactos, Producto, servicio y tienda

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('tienda/', views.tienda, name="tienda"),
+    # path('tienda/<catalogo_name>/', views.tienda_catalogo, name="tienda_catalogo"),
+    # path('tienda/<catalogo_name>/<categoria_name>', views.tienda_categoria, name="tienda_categoria"),
+    # path('distribuidores/', views.distribuidores, name="distribuidores"),
+    path('servicio/', views.servicio, name="servicio"),
+    # path('artesanos/', views.artesanos, name="artesanos"),
+    path('contactos/', views.contactos, name="contactos"),
+    path('producto/<int:producto_id>/', views.producto, name="producto"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'tienda/index.html')
+
+def tienda(request):
+    return render(request, 'tienda/tienda.html')
+
+def producto(request, producto_id):
+    return render(request, 'tienda/producto.html')
+
+def servicio(request):
+    return render(request, 'tienda/servicio.html')
+
+def contactos(request):
+    return render(request, 'tienda/contactos.html')
